4701126fc2befbab382bb2efc3c4f412158abbe0
RomanoInc <jc.molinamon@gmail.com>
2025-04-21 18:54:45 -0500

    Add files via upload

---
+++ Ejercicio1dia1.py
@@ -0,0 +1,15 @@
+
+nombre = input("Por favor ingrese nombre: ")
+apellido = input("Porfavor ingresa apellido: ")
+edad = int (input("ingrese su edad: "))
+print (nombre, apellido)
+print(edad)
+
+# siempre despues de la condicion se finaliza con dos puntos ":"
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
+
